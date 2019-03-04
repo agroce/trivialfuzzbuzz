39d4a47fe86e5abbfde82ff18823ad7484aabbcb
agroce <agroce@gmail.com>
2019-03-04 11:02:20 -0700

    fix the bug

--- harness.cpp
+++ harness.cpp
@@ -4,15 +4,23 @@
 
 // Simple fuzz target for BrokenMethod().
 extern "C" int FuzzerEntrypoint(const uint8_t *data, size_t size) {
-  if (size < 10) {
+  if (size < 20) {
     return 0;
   }
   if (data[0] > 125) {
     if (data[1] < 125) {
       if (data[2] == 42) {
-	assert(data[3] != 255);
+	if (data[3] == 41) {
+	  assert(data[3] != 255);
+	}
       }
+      if (data[2] == 181) {
+	assert(data[7] != 255);
+      }      
     }
   }
+  if ((data[9] == 12) && (data[15] == data[2])) {
+    assert(data[28] != 32);
+  }        
   return 0;
 }
