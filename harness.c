0a147f3bbfff26f19f187d74a6a28824684cb7d5
agroce <agroce@gmail.com>
2019-03-04 09:10:42 -0700

    try a very simple example

---
+++ harness.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+
+// Simple fuzz target for BrokenMethod().
+extern int FuzzerEntrypoint(const uint8_t *data, size_t size) {
+  if (size < 10) {
+    return 0;
+  }
+  if (data[0] > 125) {
+    if (data[1] < 125) {
+      if (data[2] == 42) {
+	assert(data[3] != 255);
+      }
+    }
+  }
+  return 0;
+}
